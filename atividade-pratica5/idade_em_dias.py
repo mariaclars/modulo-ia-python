96ceed283d69ac9326ebfafda31876694a2e43fa
mariaclars <mariaclarah3105@gmail.com>
2025-10-03 20:12:20 -0300

    atividade 5

---
+++ atividade-pratica5/idade_em_dias.py
@@ -0,0 +1,12 @@
+import datetime
+
+def calcular_idade_em_dias(ano_nascimento):
+    ano_atual = datetime.datetime.now().year
+    idade_anos = ano_atual - ano_nascimento
+    idade_dias = idade_anos* 365
+    return idade_dias
+
+ano_nasc = int(input("\nDigite o ano de nacimento: "))
+
+idade_em_dias = calcular_idade_em_dias(ano_nasc)
+print(f"Sua idade aproximada em dias é: {idade_em_dias} dias.")
