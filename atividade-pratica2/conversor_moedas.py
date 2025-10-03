a66d1fa04d37175ff8ae381b9b73d57405c2d658
mariaclars <mariaclarah3105@gmail.com>
2025-10-03 16:37:16 -0300

    Calculadora de descontos de Loja

---
+++ atividade-pratica2/conversor_moedas.py
@@ -0,0 +1,14 @@
+#Conversor e moedas: Real para Dólar e Euro
+  #Valores das Moedas
+valor_em_reais = float(input("Digite o valor em reais para a conversão"))
+cotacao_dolar = 5,20
+cotacao_euro = 6,15
+
+  #Conversão 
+conversao_em_dolares = valor_em_reais / cotacao_dolar
+conversao_em_euros = valor_em_reais / cotacao_euro
+
+  #Exibição dos Resultados
+print(f"Saldo em Reais: R$ {valor_em_reais}")
+print(f"Valor em Dolares: $ {conversao_em_dolares}")
+print(f"Valor em Euro £ {conversao_em_euros}")
